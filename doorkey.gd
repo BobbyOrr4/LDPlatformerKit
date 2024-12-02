2205ec039df4e8b4809eae77f67df13252dc81d3
BobbyOrr4 <viktorganzhagames@gmail.com>
2024-12-02 09:00:52 +0100

    door collision

--- doorkey.gd
+++ doorkey.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 @onready var animation_player = $AnimationPlayer
+@onready var door_blocker_collision = $DoorBlocker/CollisionShape3D  # Узел с коллизией
+
 @export var closed = true
 
 var entered = false
@@ -9,6 +11,7 @@ var opened = false
 func _process(delta):
 	if !closed and !opened: 
 		animation_player.play("Open")
+		door_blocker_collision.set_deferred("disabled", true)  # Отключение коллизии
 		opened = true  
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
